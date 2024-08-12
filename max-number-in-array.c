2be1d655a8b339ba662cc89fc8248eed7fc39ddf
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-12 08:38:41 +0500

    learning

--- max-number-in-array.c
+++ max-number-in-array.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
+int findMax(int array[], int length);
 int main(void)
 {
     int myArray[] = {8, 7, 5, 3, 9, 24, 12, 18, 27, 29, 31};
-    int max = myArray[0];
+    int myArray2[] = {2, 5, 6, 3, 7, 15, 0};
 
-    for (int i = 0; i < 11; i++)
+    int max1 = findMax(myArray, 11);
+    int max2 = findMax(myArray2, 7);
+
+    printf("max1: %d\n", max1);
+    printf("max2: %d\n", max2);
+
+    return 0;
+}
+
+int findMax(int array[], int length)
+{
+    int max = array[0];
+
+    for (int i = 1; i < length; i++)
     {
-        if (myArray[i] > max)
+        printf("myArray[%d] = %d\n", i, array[i]);
+        if (array[i] > max)
         {
-            printf("myArray[%d] = %d\n", i, myArray[i]);
-            max = myArray[i];
+
+            max = array[i];
             printf("New max found: %d\n", max);
         }
     }
-    printf("max: %d\n", max);
-
-    return 0;
+    return max;
 }
