a880aab043b440533143485f8cfe7eb17c407c62
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-22 15:29:20 +0500

    learning

--- output/sum-of-values-in-array.c
+++ output/sum-of-values-in-array.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
+int sum(int array[], int length);
+
 int main(void)
 {
-    int myArray[] = {1, 2, 6, 9, 8, 7, 6, 5};
-    int sum = 0;
+    int a1[] = {1, 2, 6, 9, 8, 7, 6, 5};
+    int a2[] = {100};
+    int a3[] = {99, 99};
+    int a4[] = {50, -50, 100, -100, 200, -200};
 
-    for (int i = 0; i < 8; i++)
-    {
-        sum = sum + myArray[i];
-        printf("myArray[%d] = %d\n", i, myArray[i]);
-        printf("sum so far: %d\n", sum);
-    }
-
-    printf("Sum of array: %d\n", sum);
+    printf("a1 sum: %d\n", sum(a1, 8));
+    printf("a1 sum: %d\n", sum(a2, 1));
+    printf("a1 sum: %d\n", sum(a3, 2));
+    printf("a1 sum: %d\n", sum(a4, 6));
 
     return 0;
+}
+
+int sum(int array[], int length)
+{
+    int sumValue = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sumValue = sumValue + array[i];
+    }
+
+    return sumValue;
 }
